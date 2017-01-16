6be1b928005584f49a64370de3298c5eb8a29fa9
Yan Huo <yan.huo@tufts.edu>
2017-01-16 14:56:47 -0500

    add skills

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// $("#info").height($("#photo").height());
+window.addEventListener("resize", setSkillHeight());
+
+function setSkillHeight() {
+	var skills = document.getElementsByClassName("skill-set");
+	for (var i = 0; i < skills.length; i++) {
+		var width = parseInt(window.getComputedStyle(skills[i]).width);
+		var height = 0.6 * width;
+		skills[i].style.height = (height + "px");
+	}      
+}
+
+// window.addEventListener("resize", function(e) {
+//     var mapElement = document.getElementById("map");
+//     mapElement.style.height = mapElement.offsetWidth * 1.72;
+// });
