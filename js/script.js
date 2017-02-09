43c3af76a0de12d73ca784a375bf0c47f1eb529f
Yan Huo <yanhuo0403@yahoo.com>
2017-02-09 16:27:14 -0500

    add projects 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// resize skills
 window.addEventListener("resize", setSkillHeight());
 
 function setSkillHeight() {
@@ -9,7 +10,30 @@ function setSkillHeight() {
 	}      
 }
 
-// window.addEventListener("resize", function(e) {
-//     var mapElement = document.getElementById("map");
-//     mapElement.style.height = mapElement.offsetWidth * 1.72;
-// });
+// hide and show projects details
+$("#project1-more").click(function() {
+     $("#project1-detail").show();
+     $("#project1-more").hide();
+});
+$("#project1-less").click(function() {
+     $("#project1-detail").hide();
+     $("#project1-more").show();
+});
+
+$("#project2-more").click(function() {
+     $("#project2-detail").show();
+     $("#project2-more").hide();
+});
+$("#project2-less").click(function() {
+     $("#project2-detail").hide();
+     $("#project2-more").show();
+});
+
+$("#project3-more").click(function() {
+     $("#project3-detail").show();
+     $("#project3-more").hide();
+});
+$("#project3-less").click(function() {
+     $("#project3-detail").hide();
+     $("#project3-more").show();
+});
